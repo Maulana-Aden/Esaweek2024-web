4dc40e73c902f5a45fd1350c5b5105c15bfe13e5
Maulana-Aden <140379200+Maulana-Aden@users.noreply.github.com>
2024-06-30 14:29:41 +0700

    Add files via upload

--- src/component/Event.js
+++ src/component/Event.js
@@ -2,21 +2,10 @@
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "../App.css";
 import "swiper/css";
-import data from "./Slide.json";
 import { slideSettings } from "./SlideSettings";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Event = () => {
-  // const [swipe, setSwipe] = useState(false);
-
-  // useEffect(() => {
-  //   if (window.clientWidth <= 720) {
-  //     setSwipe(true);
-  //   } else {
-  //     setSwipe(false);
-  //   }
-  // }, []);
-
   return (
     <div className="event" id="event">
       <div className="event-container">
@@ -27,27 +16,173 @@ const Event = () => {
         <div className="event-main">
           <Swiper {...slideSettings} loop className="swiper-main">
             <SliderButton />
-            {data.map((card, i) => (
-              <SwiperSlide key={i}>
-                <div className="r-card">
-                  <img
-                    src={card.image}
-                    alt="img"
-                    width={"400px"}
-                    height={"400px"}
-                  />
-                  <div className="event-text">
-                    <h1>{card.name}</h1>
-                    <p>{card.description}</p>
-                    <div className="event-link">
-                      <a href="https://forms.gle/zJSjvk3aEQvzfmoJA">
-                        {card.button}
-                      </a>
-                    </div>
+            <SwiperSlide>
+              <div className="r-card">
+                <img
+                  src="/img/story.png"
+                  alt="img"
+                  width={"400px"}
+                  height={"400px"}
+                />
+                <div className="event-text">
+                  <h1>Story Telling</h1>
+                  <div className="event-desc">
+                    <p>
+                      Lomba menceritakan suatu cerita secara individual untuk
+                      pelajar SMP/sederajat dan SMA/sederajat se-Jawa Tengah.
+                      Peserta diperbolehkan menggunakan alat peraga yang
+                      berhubungan dengan cerita.
+                      <br />
+                      <br />
+                      Ketentuan Lomba:
+                      <br />
+                      &nbsp; - Peserta menceritakan suatu cerita.
+                      <br />
+                      &nbsp; - Peserta diperbolehkan menggunakan alat peraga
+                      yang relevan dengan cerita.
+                    </p>
+                  </div>
+
+                  <div className="event-link">
+                    <a href="https://forms.gle/zJSjvk3aEQvzfmoJA">
+                      Daftar Sekarang
+                    </a>
                   </div>
                 </div>
-              </SwiperSlide>
-            ))}
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="r-card">
+                <img
+                  src="/img/speech.png"
+                  alt="img"
+                  width={"400px"}
+                  height={"400px"}
+                />
+                <div className="event-text">
+                  <h1>Speech</h1>
+                  <div className="event-desc">
+                    <p>
+                      Lomba pidato untuk pelajar SMA/sederajat se-Jawa Tengah
+                      dan mahasiswa/mahasiswi perguruan tinggi se-Jawa Tengah.
+                      <br />
+                      <br />
+                      Ketentuan Lomba: <br />
+                      Tingkat SMA/sederajat se-Jawa Tengah:
+                      <br />
+                      &nbsp; - Setiap peserta membawakan pidato hasil karya
+                      sendiri dengan tema yang ditentukan oleh panitia.
+                      <br />
+                      &nbsp; - Peserta menceritakan suatu cerita.
+                      <br />
+                      <br />
+                      Tingkat Perguruan Tinggi se-Jawa Tengah:
+                      <br /> &nbsp; - Setiap peserta membawakan pidato hasil
+                      karya sendiri yang dibuat pada hari pelaksanaan lomba
+                      dengan tema yang telah ditentukan oleh panitia.
+                    </p>
+                  </div>
+
+                  <div className="event-link">
+                    <a href="https://forms.gle/zJSjvk3aEQvzfmoJA">
+                      Daftar Sekarang
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="r-card">
+                <img
+                  src="/img/essay.jpeg"
+                  alt="img"
+                  width={"400px"}
+                  height={"400px"}
+                />
+                <div className="event-text">
+                  <h1>Essay</h1>
+                  <div className="event-desc">
+                    <p>
+                      Lomba menulis esai dalam bahasa Inggris untuk
+                      mahasiswa/mahasiswi se-Jawa Tengah. <br />
+                      <br />
+                      Ketentuan Lomba: <br />
+                      &nbsp; - Peserta mengumpulkan draft esai berbahasa
+                      Inggris. <br />
+                      &nbsp; - Esai akan diseleksi oleh juri hingga terpilih 10
+                      esai untuk dipresentasikan saat babak final.
+                    </p>
+                  </div>
+
+                  <div className="event-link">
+                    <a href="https://forms.gle/zJSjvk3aEQvzfmoJA">
+                      Daftar Sekarang
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="r-card">
+                <img
+                  src="/img/poetry.jpg"
+                  alt="img"
+                  width={"400px"}
+                  height={"400px"}
+                />
+                <div className="event-text">
+                  <h1>Poem Reading</h1>
+                  <div className="event-desc">
+                    <p>
+                      Lomba membaca puisi dalam bahasa Inggris untuk pelajar
+                      SMP/sederajat dan SMA/sederajat se-Jawa Tengah. <br />
+                      <br />
+                      Ketentuan Lomba:
+                      <br />
+                      &nbsp; - Peserta membacakan puisi dengan tema yang telah
+                      ditentukan oleh panitia.
+                    </p>
+                  </div>
+
+                  <div className="event-link">
+                    <a href="https://forms.gle/zJSjvk3aEQvzfmoJA">
+                      Daftar Sekarang
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="r-card">
+                <img
+                  src="/img/spelling.png"
+                  alt="img"
+                  width={"400px"}
+                  height={"400px"}
+                />
+                <div className="event-text">
+                  <h1>Spelling Bee</h1>
+                  <div className="event-desc">
+                    <p>
+                      Lomba mengeja kata untuk pelajar SMP/sederajat dan
+                      SMA/sederajat se-Jawa Tengah.
+                      <br />
+                      <br />
+                      Ketentuan Lomba:
+                      <br />
+                      &nbsp; - Peserta mengeja kata berbahasa Inggris yang sudah
+                      ditentukan oleh panitia.
+                    </p>
+                  </div>
+
+                  <div className="event-link">
+                    <a href="https://forms.gle/zJSjvk3aEQvzfmoJA">
+                      Daftar Sekarang
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
           </Swiper>
         </div>
       </div>
