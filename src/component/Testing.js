dcf7689e682a4868cb34c60203ebb7f489fe9d01
Maulana-Aden <140379200+Maulana-Aden@users.noreply.github.com>
2024-07-28 18:44:29 +0700

    Add files via upload

---
+++ src/component/Testing.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Testing = () => {
+  return (
+    <div className="testing">
+      <div className="testing-container">
+        <img
+          src="/img/element1.png"
+          alt=""
+          width={200}
+          height={200}
+          className="fairy"
+        />
+        <img
+          src="/img/element2.png"
+          alt=""
+          width={300}
+          height={300}
+          className="star"
+        />
+        <img
+          src="/img/element3.png"
+          alt=""
+          width={300}
+          height={300}
+          className="fairy2"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Testing;
